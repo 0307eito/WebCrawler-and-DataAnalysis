4cb8884e1150e399f0646cb5bc02235da573f90d
0307eito <66252302+0307eito@users.noreply.github.com>
2020-08-31 09:51:46 +0800

    2020/8/31

---
+++ homework/main.py
@@ -0,0 +1,26 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+
+def main():
+    target_urls = [
+        'https://www.ptt.cc/bbs/Gossiping/M.1559788476.A.074.html',
+        'https://www.ptt.cc/bbs/Gossiping/M.1557928779.A.0C1.html'
+    ]
+    process = CrawlerProcess(get_project_settings())
+    process.crawl('PTTCrawler', start_urls=target_urls, filename='test.json')
+    process.start()
+
+if __name__ == '__main__':
+    main()
+
+class PttcrawlerSpider(scrapy.Spider):
+    name='PTTCrawler'
+    allowed_domains=['www.ptt.cc']
+    star_urls=[]
+    def __init__(self,filename=None):
+        self.colkies={'over18':'1'}
+        self.filename=filename
+        
+
